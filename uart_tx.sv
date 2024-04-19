1b395bef0f84df6d44fbe950bd10af16fbfb5297
Nauman Iqbal <naumani@xilinx.com>
2024-04-19 13:43:13 +0100

    Minor changes

--- uart_tx.sv
+++ uart_tx.sv
@@ -1,8 +1,3 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// You can obtain one at http://mozilla.org/MPL/2.0/.
-//
-// Copyright (c) 2014-2024, Lars Asplund lars.anders.asplund@gmail.com
 
 module uart_tx(clk, tx, tready, tvalid, tdata);
    parameter integer cycles_per_bit = 434;
